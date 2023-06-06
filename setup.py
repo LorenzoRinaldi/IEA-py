e0da298644f6270f0ca707247ed3404347683ca2
Lorenzo Rinaldi <lorenzo.rinaldi13@gmail.com>
2023-06-06 15:45:41 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@author: 
+    Lorenzo Rinaldi, Department of Energy, Politecnico di Milano, Italy
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="ieapy",
+    description=(
+        "A class to read IEA balances in Excel and rearrange them in supply-use format"
+    ),
+    url="https://github.com/LorenzoRinaldi/IEA-py",
+    author="Lorenzo Rinaldi",
+    author_email="lorenzo.rinaldi@polimi.it",
+    version="0.1.0",
+    packages=find_packages(),
+    license="GNU General Public License v3.0",
+    #python_requires=">.3.7.0",
+    package_data={"": ["*.txt", "*.dat", "*.doc", "*.rst","*.xlsx"]},
+    install_requires=[
+        "mariopy >= 0.1.0",
+        ],
+    )
